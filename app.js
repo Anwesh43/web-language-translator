84857d05a874970aa9550293a2e8495b5547d60c
Anwesh43 <anweshthecool0@gmail.com>
2017-12-02 22:56:34 +0530

    testing translateApi

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ var router = require('./router')
 app.use('/app',router)
 app.listen(8000,()=>{
     console.log("listening in port 8000")
+    const translateApi = require('./translateApi')
+    translateApi.translate('hi','hello world',(sentence)=>{
+      console.log("testing the sentence is")
+      console.log(sentence)
+    })
 })
