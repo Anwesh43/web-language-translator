f9540b662770df34949a7a7303f8c21d322861e1
Anwesh43 <anweshthecool0@gmail.com>
2017-12-03 21:51:41 +0530

    adding translateAsync

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ app.use('/app',router)
 app.listen(8000,()=>{
     console.log("listening in port 8000")
     const translateApi = require('./translateApi')
-    translateApi.translate('hi','hello world',(sentence)=>{
-      console.log("testing the sentence is")
-      console.log(sentence)
-    })
+    for(var i=0;i<10;i++) {
+      translateApi.translateAsync('hi',`hello world ${i}`,(sentence)=>{
+        console.log("testing the sentence is")
+        console.log(sentence)
+      })
+    }
 })
--- translateApi.js
+++ translateApi.js
@@ -1,4 +1,6 @@
 const childProcess = require('child_process')
+const seqQueue = require('seq-queue')
+const queue = seqQueue.createQueue(1000)
 class TranslateApi {
     translate(lang,sentence,cb) {
         this.executeTranslateProcess(lang,sentence,cb)
@@ -12,6 +14,14 @@ class TranslateApi {
                     cb(lines[2])
                 }
             }
+        })
+    }
+    translateAsync(lang,sentence,cb) {
+        queue.push((task)=>{
+            this.translate(lang,sentence,(sentence)=>{
+                cb(sentence)
+                task.done()
+            })
         })
     }
 }
